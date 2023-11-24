997654e5da36b9e0b49b1da5acb2b874928dbd47
ahmedhelmy204 <ahmedhelmy204@hotmail.com>
2023-11-24 17:17:37 +0100

    seed more data

--- Conferences.Api/DAL/ConferenceContextSeed.cs
+++ Conferences.Api/DAL/ConferenceContextSeed.cs
@@ -31,6 +31,16 @@ public class ConferenceContextSeed
             new Conference()
             {
                 Id = 1,
+                Name = "XP Days " + DateTime.Now.Year,
+                Date = new DateTime(DateTime.Now.Year, 3, 14),
+                Overview = "Overview for XP Days " + DateTime.Now.Year,
+                Location = "Cairo",
+                IsPublished = true,
+                UserId= 2
+            },
+            new Conference()
+            {
+                Id = 2,
                 Name = "Tech Conference " + DateTime.Now.Year,
                 Date = new DateTime(DateTime.Now.Year, 5, 15),
                 Overview = "Overview for Tech Conference " + DateTime.Now.Year,
@@ -40,11 +50,31 @@ public class ConferenceContextSeed
             },
             new Conference()
             {
-                Id = 2,
+                Id = 3,
                 Name = "Data Summit",
                 Date = new DateTime(DateTime.Now.Year, 8, 20),
                 Overview = "Overview for Data Summit",
                 Location = "Cairo",
+                IsPublished = false,
+                UserId= 2
+            },
+            new Conference()
+            {
+                Id = 4,
+                Name = "AI and Chat GPT",
+                Date = new DateTime(DateTime.Now.Year, 2, 16),
+                Overview = "Overview for AI and Chat GPT",
+                Location = "Berlin",
+                IsPublished = false,
+                UserId= 2
+            },
+            new Conference()
+            {
+                Id = 5,
+                Name = "The Machine Learning Conference",
+                Date = new DateTime(DateTime.Now.Year, 1, 23),
+                Overview = "Overview for The Machine Learning Conference",
+                Location = "London",
                 IsPublished = false,
                 UserId= 2
             }
